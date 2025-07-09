c722ae1701b399580b193906743314a4de3e18ea
Eric Mueller <nevinera@gmail.com>
2025-07-09 11:35:15 -0400

    build into /docs

--- config.rb
+++ config.rb
@@ -10,6 +10,8 @@ end
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
 
+set :build_dir, "docs"
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
